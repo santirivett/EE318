fc644eb02aac5510199bba0c612eaae1dfc041d9
santirivett <98153843+santirivett@users.noreply.github.com>
2024-02-16 12:37:59 +0000

    Updated 318code.c

--- 318code.c
+++ 318code.c
@@ -1,25 +1,48 @@
 #include "318code.h"
 
+volatile unsigned int adcValue = 0;
+
+long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 
 void setupadc()
 {
     //Step 1: Pin  Select P8.1 for ADC function
 
     GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P8, GPIO_PIN1, GPIO_PRIMARY_MODULE_FUNCTION);
-    P8SEL0 |= BIT1;
+    //P8SEL0|= BIT1;
+     P1SEL0 |= BIT1;
 
    //Step 2: Configure CLK source
 
-    ADCCTL0 |= ADCSHT_2 | ADCON;               // Sample and hold time, ADC on
-    ADCCTL1 |= ADCSSEL_2 | ADCDIV_0 | ADCSHP; // Clock source: SMCLK, Clock divider: 1, Pulse-mode
+    ADCCTL0 |= ADCSHT_2 | ADCON;               // Sample and hold time, ADC on, 16 ADCCLK cycles
+    ADCCTL1 |= ADCSSEL_2|ADCSHP| ADCCONSEQ_1; // Clock source: SMCLK, Clock divider: 1, Pulse-mode
     ADCCTL2 |= ADCRES;                        // Resolution: 10-bit
-    ADCMCTL0 |= ADCINCH_9;                    // Select A0 for ADC input channel
+    ADCMCTL0 |= ADCINCH_9;                    // Select A9 for ADC input channel
 
     // Enable ADC interrupt
 
-    ADCIE |= ADCIE0;                         // Enable ADC interrupt for channel 0
-   __enable_interrupt();                     // Enable global interrupts
+    //ADCIE |= ADCIE0;                         // Enable ADC interrupt for channel 0
+  // __enable_interrupt();     // Enable global interrupts
+}
 
+
+
+// ADC ISR
+#pragma vector=ADC_VECTOR
+__interrupt void ADC_ISR(void)
+{
+    switch (__even_in_range(ADCIV, ADCIV_ADCIFG))
+    {
+        case ADCIV_ADCIFG:                    // ADC conversion completed
+            adcValue = ADCMEM0;               // Store ADC value
+            break;
+        default:
+            break;
+    }
 }
 
 
@@ -37,7 +60,7 @@ void configureMotor ()
     // __delay_cycles(2000000); // Delay to reach position
 
 
-
+//Code below. 180 rotation
 
      /*while(1)
          {
@@ -58,6 +81,7 @@ void configureMotor ()
          */
 }
 
+// Later on implement switch on joystick to do this implementation
 
 void setzeroposition()
 {
@@ -68,19 +92,32 @@ void setzeroposition()
 
 
     TA0CCR0=20000;  //PWM (20ms)
-    TA0CCR1=1000;
+    TA0CCR1=1500;
     TA0CCTL1 |= OUTMOD_7;
     TA0CTL = TASSEL_2 + MC_1;
 
 }
 
-SetServoPosition(int x)
+void SetServoPosition(int x)
 {
-    TA0CCR1=x;
+    int duty_cycle = (x * 1000 / 180) + 1000;
+    TA0CCR1=duty_cycle;
 }
 
 
 
+
+
+/*void joystickRead(unsigned int *xAxis)
+{
+    ADCCTL0&= ~ADCSC;
+    ADCCTL0|=ADCSC;
+
+    while(!(ADCIFG & ADCIFG1));     // wait for ADC conversion to complete
+
+        *xAxis = ADCMEM0;           // store memory location
+
+}*/
 
 
 
