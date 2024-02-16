73544a0348d51a430a0e7e3a5d48485602baa1a3
santirivett <98153843+santirivett@users.noreply.github.com>
2024-02-16 12:38:29 +0000

    Updated 318code.h

--- 318code.h
+++ 318code.h
@@ -3,6 +3,13 @@
 #include <driverlib.h>
 
 void configureMotor();
+
 void setzeroposition();
+
 void setupadc();
-SetServoPosition(int x);
+
+void SetServoPosition(int x);
+
+long map(long x, long in_min, long in_max, long out_min, long out_max);
+
+void joystickRead(unsigned int *xAxis);
