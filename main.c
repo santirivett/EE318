a1e3ee30fe1b97b802bd9374c885bc77eb47d09d
santirivett <98153843+santirivett@users.noreply.github.com>
2024-02-16 12:37:37 +0000

    Updated main.c

--- main.c
+++ main.c
@@ -1,96 +1,33 @@
 #include "318code.h"
 
-
 #define ADC_THRESHOLD        1000    // Threshold value to detect joystick movement
+#define JOYSTICK_CENTER      512     // Center position of the joystick
+#define SERVO_MIN            0       // Minimum servo position
+#define SERVO_MAX            180     // Maximum servo position
+#define SERVO_CENTER         90      // Center position for the servo
 
-// ADC interrupt service routine
-
-   #pragma vector=ADC_VECTOR
-   __interrupt void ADC_ISR(void)
-   {
-       switch (__even_in_range(ADCIV, ADCIV_ADCIFG))
-       {
-           // ADC conversion complete
-           case ADCIV_ADCIFG:
-               adc_value = ADCMEM0; // Store ADC value
-           break;
-       }
-   }
-
-
-
+ int joystickValue = 0;
+ int servoPosition =0;
 void main(void)
 {
-   //setzeroposition();    // start at 0 degrees
-     setupadc();            // Initilaise ADC
-    configureMotor();
+    setupadc();            // Initialise ADC
+    configureMotor();      // Initialise servo motor control
+    PM5CTL0&=~LOCKLPM5;
 
     while(1)
-        {
-            ADCCTL0 |= ADCENC + ADCSC;             // Sampling and conversion start
-            while(ADCCTL1 & ADCBUSY);           // Wait until conversion is done
-
-            unsigned int adcValue = ADCMEM0;       // Read ADC value
-
-            if (adcValue < ADC_THRESHOLD)           // Joystick not moved
-            {
-                SetServoPosition(500);  // Keep servo at left position
-            }
-            else if (adcValue > 1023 - ADC_THRESHOLD)
-            {
-                SetServoPosition(2500); // Keep servo at right position
-            }
-            else
-            {
-                // Joystick in neutral position, do nothing
-            }
-        }
-
-
-}
-
-
-
-
-
-
-
-   /* while(1)
     {
-        // aim to continuously monitor and update servo position with joystick
+        // Read joystick input
+        ADCCTL0 |= ADCENC | ADCSC;  //  ADC conversion
+        while((ADCIFG & ADCIFG0)==0);    // Wait for conversion to complete
 
-        if(adc_value<(ADC_THRESHOLD -JOYSTICK_DEADZONE))
-        {
-            //LEFT
-            configureMotor(SERVO_MAX_VALUE);
-            //TA0CCR1=SERVO_MAX_VALUE;
-            joystick_active = 1; // Joystick is active
-        }
+        joystickValue = ADCMEM0;
 
-        else if(adc_value>(ADC_THRESHOLD + JOYSTICK_DEADZONE))
-        {
-            //RIGHT
-            configureMotor(SERVO_MIN_VALUE);
+        // Map joystick input to servo position (0-180 degrees)
+        servoPosition = map(joystickValue, 0, 1023, SERVO_MIN, SERVO_MAX);
 
-           // TA0CCR1= SERVO_MIN_VALUE;
-            joystick_active = 1; // Joystick is active
-
-        }
-        else
-        {
-            if((joystick_active)==0)
-            {
-                //go to zero position
-
-                 setzeroposition();
-                 joystick_active=0;
-            }
-        }
-    }*/
-//}
-
-
-
-
+        // Set servo position by utilising PWM duty cycles
+        TA0CCR1 = (servoPosition * 1000 / 180) + 1000;
+    }
+}
 
 
